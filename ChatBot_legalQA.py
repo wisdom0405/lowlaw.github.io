5156fe1fc258d5a90fb9a68a09f00a2fa6a26b1a
wisdom <wisdom0405@kakao.com>
2023-06-07 14:22:07 +0900

    lowlaw1

---
+++ ChatBot_legalQA.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import requests
+
+#@st.cache_data(persist="disk")
+#@st.cache(allow_output_mutation=True)
+@st.cache_data()
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+#@st.cache_data(persist="disk")
+#@st.cache(allow_output_mutation=True)
+@st.cache_data()
+def get_dataset():
+    df = pd.read_csv('/Users/wisdom/Documents/데이터분석/ChatBot_legalQA/legalQA_embedding.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+st.title('LowLaw에게 물어보기')
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
+
+if submitted and user_input:
+    embedding = model.encode(user_input)
+
+    df['similarity'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['similarity'].idxmax()]
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['answer'])
+
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
